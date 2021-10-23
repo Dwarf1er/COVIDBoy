2e838700dba95ac87dc5e3cde57f5325efd38b7a
Dwarf1er <poulin.antoine@hotmail.com>
2021-10-22 21:36:58 -0400

    Added ennemy sprites and fixed the game over menu

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -12,8 +12,8 @@ public class GameOverMenu : MonoBehaviour
         restartButton = GameObject.Find("RestartButton").GetComponent<Button>();
         mainMenuButton = GameObject.Find("MainMenuButton").GetComponent<Button>();
 
-        restartButton.onClick.AddListener(() => Debug.Log("I have been clicked"));
-        mainMenuButton.onClick.AddListener(() => SceneManager.LoadScene("MainMenu"));
+        restartButton.onClick.AddListener(() => SceneManager.LoadScene("Game"));
+        mainMenuButton.onClick.AddListener(() => SceneManager.LoadScene("Menu"));
     }
 
     public void ActivateGameOverMenu()
